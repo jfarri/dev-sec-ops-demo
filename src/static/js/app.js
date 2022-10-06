73651f30c0d03d876cbc92416f61a010b1b643ff
farri <farri>
2022-10-06 02:30:01 +0000

    test 3

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Please add only one above. Green</p>
+                <p className="text-center">No items yet! Please add only blue above. Green</p>
             )}
             {items.map(item => (
                 <ItemDisplay
