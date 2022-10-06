ac392c484498427e546b3d126b0ed6eb69b6b3fa
farri <farri>
2022-10-06 12:23:58 +0000

    alpha1

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Please add only alpha above. Green</p>
+                <p className="text-center">No items yet! Please add only alpha1 above. Green</p>
             )}
             {items.map(item => (
                 <ItemDisplay
